1c0cbee6e8e6437ef60e99eca795a4819d4e7e00
Sanjay <sanjay@users.noreply.github.com>
2026-09-21 12:11:44 +0530

    Add push_to_github.bat helper script

---
+++ push_to_github.bat
@@ -0,0 +1,5 @@
+@echo off
+set "PATH=C:\Users\sanja\AppData\Local\Microsoft\WinGet\Packages\Git.MinGit_Microsoft.Winget.Source_8wekyb3d8bbwe\cmd;%PATH%"
+echo Pushing to GitHub (https://github.com/Sanjjuhh/caltrack.git)...
+git push -u origin main
+pause
